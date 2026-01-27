b8e556f9c75e767e77c846f92d16fded59496e7c
Dmitriy <soapman@bk.ru>
2026-01-27 10:59:51 +0300

    11

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = process.env.API_URL || 'http://localhost:5000';
+const API_URL = 'https://car-status-backend.onrender.com';
 export default API_URL;
 
 let token = localStorage.getItem('token') || '';
