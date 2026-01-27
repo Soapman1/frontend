49696414ea8ae3bc4baa0d06e7b22bba45f89fee
Dmitriy <soapman@bk.ru>
2026-01-27 11:18:51 +0300

    fixed local host

--- src/pages/Track.jsx
+++ src/pages/Track.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'https://car-status-backend.onrender.com';
 
 function Track() {
   const [searchPlate, setSearchPlate] = useState('');
